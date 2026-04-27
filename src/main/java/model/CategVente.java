0586f70e6641890b585e997cf832a0b1baf18712
sio2 <antoninfouchet@gmail.com>
2026-04-27 13:29:51 +0200

    Modification de CategVente pour récupérer les catégories

--- src/main/java/model/CategVente.java
+++ src/main/java/model/CategVente.java
@@ -1,56 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author sio2
- */
 public class CategVente {
 
-    private int code;
+    private String code;
     private String libelle;
-    
+
     private ArrayList<Vente> lesVentes;
-    
+
     public CategVente(){
-    } 
-
-    public int getCode() {
-        return code;
     }
 
-    public String getLibelle() {
-        return libelle;
-    }
+    public String getCode() {return code;}
 
-    public void setCode(int code) {
-        this.code = code;
-    }
+    public String getLibelle() {return libelle;}
 
-    public void setLibelle(String libelle) {
-        this.libelle = libelle;
-    }
+    public void setCode(String code) {this.code = code;}
 
-        
-    public ArrayList<Vente> getLesVentes() {
-        return lesVentes;
-    }
+    public void setLibelle(String libelle) {this.libelle = libelle;}
 
-    public void setLesVentes(ArrayList<Vente> lesVentes) {
-        this.lesVentes = lesVentes;
-    }
+    public ArrayList<Vente> getLesVentes() {return lesVentes;}
+
+    public void setLesVentes(ArrayList<Vente> lesVentes) {this.lesVentes = lesVentes;}
+
     public void addVente(Vente uneVente){
-        if (lesVentes ==null ){
+        if (lesVentes == null){
             lesVentes = new ArrayList<Vente>();
         }
         lesVentes.add(uneVente);
     }
-
-    
-    
 }
